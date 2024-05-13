add1c390363b6da57a788f7611d2b207bdeebcfb
jinaldavra <davrajinal63@gmail.com>
2024-05-13 05:38:31 -0700

    Add operator

---
+++ Loop/Operator/Binary.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int value1 = 7, value2 = 14;
+	clrscr();
+
+       //	int ans = value1 & value2;
+       //	printf("%d",ans);          //ans is binary of 6
+
+       //	int ans = value1 | value2;
+       //	printf("%d",ans);         //ans is binary of 15
+
+       //	int ans = value1 ^ value2;
+       //	printf("%d",ans);         //ans is binary of 9
+
+       //	int ans = value1 << 1;
+       //	printf("%d",ans);   //ans is binary of 14
+
+       //	int ans = value1 >> 1;
+       //	printf("%d",ans);   //ans is binary of 3
+
+     //         int ans = value2 << 1;
+      //        printf("%d",ans);   //ans is binary of 28
+
+	int ans = value2 >> 1;
+	printf("%d",ans);   //ans is binary of 7
+
+
+
+	getch();
+}
