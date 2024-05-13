ce7c19a1bb97e1255e7f9ffbaca631ab40dcfe7b
jinaldavra <davrajinal63@gmail.com>
2024-05-13 05:31:57 -0700

    Add operator

---
+++ Loop/Operator/Logical.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int con1 = 0; //F
+	int con2 = 1; //T
+	int con3 = 50 < 60; //T
+	int con4 = 50 == 50; //T
+	int con5 = 100 > 50; //T
+	clrscr();
+
+       //	int ans = con1 && con2 && con3;
+       //	printf("%d",ans);
+
+       //	int ans = !con1 && con2 && con3;
+       //	printf("%d",ans);
+
+       //	int ans = con1 && con5 && con3;
+       //	printf("%d",ans);
+
+       //	int ans = con1 || con5 || con3;
+       //	printf("%d",ans);
+
+       //	int ans = con1 && con2 && con3 && con4 && con5;
+       //	printf("%d",ans);
+
+       //	int ans = !con2 && con3 && con4;
+       //	printf("%d",ans);
+
+       //	int ans = !con1 || con2 || con3;
+       //	printf("%d",ans);
+
+       //	int ans = !con2 || con3 || con4;
+       //	printf("%d",ans);
+
+
+
+
+
+
+
+	getch();
+
+
+}
