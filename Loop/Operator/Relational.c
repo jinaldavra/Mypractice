62b46fc7a3bd8b0e22bfbd9a57a90b07dd159136
jinaldavra <davrajinal63@gmail.com>
2024-05-13 05:29:58 -0700

    Add operator

---
+++ Loop/Operator/Relational.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a = 30,b = 40, c = 30;
+	clrscr();
+
+	//If answer is 0 then condition will be false while answer is 1 then condition will be true..
+
+       //	printf("%d",a == b);
+
+       //	printf("%d",a != b);
+
+       //	printf("%d",a < b);
+
+       //	printf("%d",a > b);
+
+       //       printf("%d",a >= b);
+
+       //	printf("%d",a <= b);
+
+       //	printf("%d",a == b == c);
+
+       //       printf("%d",a == c);
+
+       //	printf("%d",a != c);
+
+       //	printf("%d",a < c);
+
+       //	printf("%d",a > c);
+
+       //	printf("%d",a >= c);
+
+       //	printf("%d",a <= c);
+
+       //	printf("%d",b == c);
+
+       //	printf("%d",b != c);
+
+       //	printf("%d",b < c);
+
+       //	printf("%d",b > c);
+
+       //	printf("%d",b <= c);
+
+	printf("%d",b >= c);
+
+
+
+
+
+	getch();
+
+}
