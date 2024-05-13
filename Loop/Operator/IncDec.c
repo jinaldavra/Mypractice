6d9c107df7bf3d911a852bf3fa868b2465a3e74e
jinaldavra <davrajinal63@gmail.com>
2024-05-13 05:33:25 -0700

    Add operator

---
+++ Loop/Operator/IncDec.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a = 20;
+	clrscr();
+	// Increment or Decrement
+
+       //	a++;
+       //	printf("%d",a);
+
+       //	a--;
+       //	printf("%d",a);
+
+       //	++a;
+       //	printf("%d",a);
+
+	--a;
+	printf("%d",a);
+
+
+	getch();
+}
