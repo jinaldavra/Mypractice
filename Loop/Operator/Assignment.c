d471922c929aed6c97a5bda67639465cbcdbf425
jinaldavra <davrajinal63@gmail.com>
2024-05-13 05:28:13 -0700

    Add operator

---
+++ Loop/Operator/Assignment.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a = 50;
+	clrscr();
+
+       //	a = a + 50;
+       //	printf("sum = %d",a);
+
+		       //	OR
+
+       //	a += 50;
+       //	printf("sum = %d\n",a);
+
+       //	a -= 50;
+       //	printf("sub = %d",a);
+
+       //	a *= 50;
+       //	printf("multi = %d",a);
+
+       //	a /= 50;
+       //	printf("div = %d",a);
+
+	a %= 50;
+	printf("mod = %d",a);
+
+	getch();
+}
