e2c793b329ac9d3000a85d88f405dab2410535a0
jinaldavra <davrajinal63@gmail.com>
2024-05-23 10:12:01 -0700

    Add structure and union

---
+++ .vscode/Structure & Union/union.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+union book{
+    char name[10];
+    int page;
+    float price;
+};
+int main(){
+    union book b1,b2;
+    strcpy(b1.name,"Python");
+    b1.page = 600;
+    b1.price = 300.24;
+    
+    printf("Book name : %s\n",b1.name);
+    printf("Book page : %d\n",b1.page);
+    printf("Book price : %.2f\n",b1.price);
+    
+    strcpy(b2.name,"Java");
+    b2.page = 700;
+    b2.price = 400.30;
+    
+    printf("Book name : %s\n",b2.name);
+    printf("Book page : %d\n",b2.page);
+    printf("Book price : %.2f\n",b2.price);
+    return 0;
+}
