fa7fc453c60aa6db2363a6e57a630f0bbe42c4dd
jinaldavra <davrajinal63@gmail.com>
2024-05-29 02:43:23 -0700

    Add string code

--- .vscode/String/strcat.c
+++ .vscode/String/strcat.c
@@ -11,3 +11,20 @@ int main(){
     printf("%s",name1);
     return 0;
 }
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char name1[10] = "I";
+    char s[] = " ";
+    char name2[] = "am";
+    char name3[] = "a";                       // print sentence using string
+    char name4[] = "girl";
+    strcat(name1,s);
+    strcat(name1,name2);
+    strcat(name1,s);
+    strcat(name1,name3);
+    strcat(name1,s);
+    strcat(name1,name4);
+    printf("%s",name1);
+}
