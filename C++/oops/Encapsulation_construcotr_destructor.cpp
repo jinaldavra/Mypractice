87b7cd067cd342899860a8b6069ec421065d2b99
jinaldavra <davrajinal63@gmail.com>
2024-06-03 23:37:31 -0700

    Add constructor and destructor

--- C++/oops/Encapsulation_construcotr_destructor.cpp
+++ C++/oops/Encapsulation_construcotr_destructor.cpp
@@ -32,3 +32,51 @@ int main(){
     // c2.info();
     return 0;
 }
+
+
+#include<iostream>
+using namespace std;
+class Student{
+    // data member
+    public :
+        string Name;
+        string Course;
+        int Marks;
+    // Constructor
+    Student(string n,string c,int m){
+        Name = n;
+        Course = c;
+        Marks = m;
+        cout<<"Name   : "<<Name<<endl;
+        cout<<"Course : "<<Course<<endl;
+        cout<<"Marks  : "<<Marks<<endl;
+    }
+};
+int main(){
+    Student s1("Ninja Coder","DSA in C++",99);
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+class Student{
+    // data member
+    public :
+        string Name;
+        string Course;
+    // constructor
+    Student(string n,string c){
+        Name = n;
+        Course = c;
+        cout<<"Name   : "<<Name<<endl;
+        cout<<"Course : "<<Course<<endl;
+    }
+    // destructor
+    ~Student(){
+        cout<<"I am a destructor"<<endl;
+    }
+};
+int main(){
+    Student info("Dhruv Rawat","Information Technology");
+    return 0;
+}
