81c43842b28d0a85bb324a1484ab88763b157c4c
jinaldavra <davrajinal63@gmail.com>
2024-07-11 22:02:57 +0530

    Add conditional statement

---
+++ python/conditional_statement.py/if.py
@@ -0,0 +1,20 @@
+x = 10
+
+#if x > 5 :
+#    print("X is greater than 5")
+
+# Nested if statement
+
+#if x > 5 :
+#    print("X is greater than 5")
+#    if x > 10 :
+#        print("X is greater than 10")
+
+balance = 10000
+withdraw = 5000
+
+if withdraw <= balance :
+    balance -= withdraw
+    print(balance)
+    print("withdrawl successfull")
+        
