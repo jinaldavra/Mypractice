efcbb7964bec58f3615c9958ab41da9c4f1c3dfb
jinaldavra <davrajinal63@gmail.com>
2024-07-15 21:53:52 +0530

    Add change

--- python/Assignment2/question13.py
+++ python/Assignment2/question13.py
@@ -1,14 +1,10 @@
 # 13. Write a program to find the largest number in a list using a loop.
 
-list = [23,56,45,80,46,34]
+num = [23,45,67,56,78,34,12]
+max = 0
 
-if not list:
-    largest_num = None
-else:
-    largest_num = list[0]
-
-for i in list:
-    if i > largest_num:
-        largest_num = i
-
-print(f"The largest number is {largest_num}")
+for i in num:
+    if i > max:
+        max = i
+        
+print(max)
