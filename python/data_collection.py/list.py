da86f4d97421b0c2ffc2286e96eb5c03ba0c3047
jinaldavra <davrajinal63@gmail.com>
2024-07-16 22:26:37 +0530

    Add list

---
+++ python/data_collection.py/list.py
@@ -0,0 +1,125 @@
+# list = ['apple','banana','cherry']
+
+# Accessing list:
+# print(list)
+
+# find the length of the list:
+# print(len(list))
+
+# print(type(list))
+
+# Access elements from the list using indexing(+/-):
+# print(list[0]
+# print(list[1])
+# print(list[2])
+
+# print(list[-1])
+# print(list[-2])
+# print(list[-3])
+
+# access elements from the list using slice(+/-):
+# print(list[0:2])
+# print(list[0:1])
+
+# print(list[-2:0:-1])
+# print(list[::-2])
+# print(list[::2])
+
+# mummy_list = ['apple','banana']
+# my_list = ['drink','chocolate','apple']
+
+# print(mummy_list + my_list)
+# print(mummy_list*2)
+# print(mummy_list*3)
+
+# Updating list elements:
+# list[0] = 'orange'
+# print(list)
+
+# Adding elements to list:
+
+# list methods:
+# print(dir(list))
+
+num = [1,2,3,4,5]
+new_list = [6,7,8]
+
+# add element at the end of the list:
+# num.append(new_list)
+# print(num)
+
+# new_list.append(num)
+# print(new_list)
+
+# extend list by adding elements from another list:
+# num.extend(new_list)
+# print(num)
+
+# new_list.extend(num)
+# print(new_list)
+
+# insert element at the specified position:
+# num.insert(2,new_list)
+# print(num)
+
+new_list.insert(7,num)
+print(new_list)
+
+# remove
+# num.clear()
+# print(num)
+
+# new_list.clear()
+# print(new_list)
+
+
+# remove last element from list:
+# num.pop()
+# print(num)
+
+# new_list.pop()
+# print(new_list)
+
+# num.remove(3)
+# print(num)
+
+# new_list.remove(7)
+# print(new_list)
+
+# del num[0]
+# print(num)
+
+# del new_list[0]
+# print(new_list)
+
+# copied_list = num.copy
+# print(num)
+# print(copied_list)      output : <built-in method copy of list object at 0x000001A79628D900>
+# print(id(num))
+# print(id(copied_list))
+
+# num = [1,2,1,2,1,2,3,4,5,2,1,2,3]
+
+# print(num.count(1))
+# print(num.count(2))
+# print(num.count(3))
+# print(num.count(4))
+# print(num.count(5))
+
+# print(num.index(4))
+# print(num.index(5))
+
+# num.sort()
+# print(num)
+
+# num.reverse()
+# print(num)
+
+# mix = [10,20.3,45+3j],['precious','mindset'] 
+# print(mix)
+
+
+
+
+
+
